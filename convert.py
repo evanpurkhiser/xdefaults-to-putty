ffa6b581f1a969dc66cebc3cf5b167d916b47653
Evan Purkhiser <evanpurkhiser@gmail.com>
2012-02-24 23:35:02 -0500

    Added a map for the colors from the xdefaults to the putty color codes

--- convert.py
+++ convert.py
@@ -1,6 +1,28 @@
 #!/usr/bin/python
 import sys, re
 
+# Color map from xefault color code => Putty color code
+color_map = {
+	'foreground': ('color0', 'color1'),
+	'background': ('color2', 'color3'),
+	'color0':     'color6',
+	'color8':     'color7',
+	'color1':     'color8',
+	'color9':     'color9',
+	'color2':     'color10',
+	'color10':    'color11',
+	'color3':     'color12',
+	'color11':    'color13',
+	'color4':     'color14',
+	'color12':    'color15',
+	'color5':     'color16',
+	'color13':    'color17',
+	'color6':     'color18',
+	'color14':    'color19',
+	'color7':     'color20',
+	'color15':    'color21'
+}
+
 # Read the passed Xdefaults file
 colors = open(sys.argv[1], 'r').readlines()
 
