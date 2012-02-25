a89bdfbc765ddbac64cc33bc27abb16c536a216b
Evan Purkhiser <evanpurkhiser@gmail.com>
2012-02-25 01:46:30 -0500

    Added support for the cursor color

--- convert.py
+++ convert.py
@@ -22,31 +22,32 @@ new_colors = ""
 
 # Color map from xefault color code => Putty color code (possible multiple)
 color_map = {
-	'foreground': ['Colour0', 'Colour1'],
-	'background': ['Colour2', 'Colour3'],
-	'color0':     ['Colour6'],
-	'color8':     ['Colour7'],
-	'color1':     ['Colour8'],
-	'color9':     ['Colour9'],
-	'color2':     ['Colour10'],
-	'color10':    ['Colour11'],
-	'color3':     ['Colour12'],
-	'color11':    ['Colour13'],
-	'color4':     ['Colour14'],
-	'color12':    ['Colour15'],
-	'color5':     ['Colour16'],
-	'color13':    ['Colour17'],
-	'color6':     ['Colour18'],
-	'color14':    ['Colour19'],
-	'color7':     ['Colour20'],
-	'color15':    ['Colour21']
+	'foreground':  ['Colour0', 'Colour1'],
+	'background':  ['Colour2', 'Colour3'],
+	'cursorColor': ['Colour4', 'Colour5'],
+	'color0':      ['Colour6'],
+	'color8':      ['Colour7'],
+	'color1':      ['Colour8'],
+	'color9':      ['Colour9'],
+	'color2':      ['Colour10'],
+	'color10':     ['Colour11'],
+	'color3':      ['Colour12'],
+	'color11':     ['Colour13'],
+	'color4':      ['Colour14'],
+	'color12':     ['Colour15'],
+	'color5':      ['Colour16'],
+	'color13':     ['Colour17'],
+	'color6':      ['Colour18'],
+	'color14':     ['Colour19'],
+	'color7':      ['Colour20'],
+	'color15':     ['Colour21']
 }
 
 # Iterate over the colors in the Xdefaults file
 for line in colors:
 
 	# Match the color value and name
-	color = re.match(r"\*(.+):.*#(.{6})", line)
+	color = re.search(r"\*(.+):.*#(.{6})", line)
 
 	# Make sure the color code exists in the color map
 	if not color or not color.group(1) in color_map:
